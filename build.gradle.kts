bb16737e565b77bd74fe63ed14c1abc3f4c074ba
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-01-17 10:09:42 -0600

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.serialization") version "1.8.0"
+    `maven-publish`
+    application
+}
+
+group = "co.q64.faktorio"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+
+    // ktor
+    implementation("io.ktor:ktor-server-core:2.2.2")
+    implementation("io.ktor:ktor-server-netty:2.2.2")
+    implementation("io.ktor:ktor-server-auth:2.2.2")
+    implementation("io.ktor:ktor-server-content-negotiation:2.2.2")
+
+    // serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "deotime"
+            url = uri("https://repo.q64.io/deotime")
+            credentials {
+                username = "deotime"
+                password = System.getenv("RAIN_REPO_PW")
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/co/q64/faktorio/Main.kt
@@ -0,0 +1,5 @@
+package co.q64.faktorio
+
+fun main(args: Array<String>) {
+
+}
