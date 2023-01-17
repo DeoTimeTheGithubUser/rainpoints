492a91a02706e0b06dbb0e95fee91de21c05eded
BuildTools <DeoTimeTheGithubUser@users.noreply.github.com>
2023-01-17 11:45:50 -0600

    Update model

--- src/main/kotlin/co/q64/faktorio/Test.kt
+++ src/main/kotlin/co/q64/faktorio/Test.kt
@@ -1,5 +1,6 @@
 package co.q64.faktorio
 
+import co.q64.faktorio.model.endpoint
 import io.ktor.server.application.call
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
@@ -7,14 +8,21 @@ import io.ktor.server.response.respond
 import io.ktor.server.routing.get
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
+import io.ktor.util.pipeline.PipelineContext
 import kotlinx.coroutines.runBlocking
 
-fun main(args: Array<String>): Unit = runBlocking {
+fun main(): Unit = runBlocking {
     embeddedServer(Netty, port = 9123) {
         routing {
             route("/test") {
                 endpoint {
-                    description = "Some description"
+                    call {
+                        val test by parameter<String>()
+
+                        request {
+
+                        }
+                    }
                 }
             }
         }
--- src/main/kotlin/co/q64/faktorio/model/Endpoint.kt
+++ src/main/kotlin/co/q64/faktorio/model/Endpoint.kt
@@ -1,29 +1,54 @@
 package co.q64.faktorio.model
 
 import io.ktor.http.HttpMethod
+import io.ktor.server.application.ApplicationCall
+import io.ktor.server.application.call
+import io.ktor.server.plugins.MissingRequestParameterException
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.createRouteFromPath
+import io.ktor.util.pipeline.PipelineContext
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.serializer
 import kotlin.reflect.KProperty
 import kotlin.reflect.KType
+import kotlin.reflect.typeOf
+
+typealias Request = PipelineContext<*, ApplicationCall>
 
 class Endpoint(
     private val route: Route,
     var description: String? = null,
     var method: HttpMethod = HttpMethod.Get,
     var scope: APIScope? = null,
-    var call: Call? = null
+    private var call: Call? = null
 ) {
+
+    fun call(closure: Call.() -> Unit) {
+
+    }
+
     fun build() {
         TODO()
     }
 
     class Call(
-        private val parameters: MutableList<Parameter<*>> = mutableListOf()
+        @PublishedApi internal val parameters: MutableList<Parameter<*>> = mutableListOf(),
+        private var request: (Request.() -> Unit)? = null
     ) {
+        inline fun <reified T : Any> parameter(
+            name: String? = null,
+            type: Parameter.Type = Parameter.Type.QueryParameter,
+            description: String? = null
+        ) =
+            Parameter<T>(typeOf<T>(), type, name, description).also { parameters += it }
+
+        fun request(closure: Request.() -> Unit) {
+            request = closure
+        }
 
     }
 
-    data class Parameter<T>(
+    data class Parameter<T> @PublishedApi internal constructor(
         val type: KType,
         val paramType: Type,
         val name: String? = null,
@@ -33,8 +58,17 @@ class Endpoint(
         operator fun provideDelegate(ref: Any?, prop: KProperty<*>) =
             name?.let { this } ?: copy(name = prop.name)
 
-//        context(Request) // todo implement
-        operator fun getValue(ref: Any?, prop: KProperty<*>): T = TODO()
+
+        // todo please make this cleaner
+        context(PipelineContext<*, ApplicationCall>) @Suppress("UNCHECKED_CAST")
+        operator fun getValue(ref: Any?, prop: KProperty<*>): T =
+            ((if (paramType == Type.QueryParameter) call.request.queryParameters[name!!]
+            else call.parameters[name!!])?.let {
+                Json.decodeFromString(serializer(type), it) // todo cache this with the call somehow
+            } as T?) ?: throw MissingRequestParameterException(name)
+
+        operator fun getValue(ref: Any?, prop: KProperty<*>): T =
+            error("Value of parameter $name can only be accessed within a request.")
 
         enum class Type {
             Parameter,
